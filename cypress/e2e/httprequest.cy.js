8a5cb76b35ad4468a65fafe8de5fd0e281003af5
Amrita Matta <matta.amrita@gmail.com>
2025-11-12 21:47:36 +0200

    feat:delete http method
    Signed-off-by: Amrita Matta <matta.amrita@gmail.com>

--- cypress/e2e/httprequest.cy.js
+++ cypress/e2e/httprequest.cy.js
@@ -1,15 +1,15 @@
 describe("HTTP Requests", () => {
-  it("GET request", () => {
-    cy.request("GET", "https://jsonplaceholder.typicode.com/users").then(
-      (response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.length(10);
-        expect(response.body[0].name).to.eq("Leanne Graham");
-      }
-    );
+  const baseUrl = "https://jsonplaceholder.typicode.com";
+
+  it("GET /users returns list of users", () => {
+    cy.request("GET", `${baseUrl}/users`).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.length(10);
+      expect(response.body[0].name).to.eq("Leanne Graham");
+    });
   });
 
-  it("POST CALL", () => {
+  it("POST /posts creates a new post", () => {
     const requestBody = {
       title: "My Test Post",
       body: "This is a test post created via Cypress.",
@@ -18,42 +18,56 @@ describe("HTTP Requests", () => {
 
     cy.request({
       method: "POST",
-      url: "https://jsonplaceholder.typicode.com/posts",
+      url: `${baseUrl}/posts`,
       body: requestBody,
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
       },
     }).then((response) => {
+      // JSONPlaceholder may return 201 for created resources
       expect([200, 201]).to.include(response.status);
       expect(response.body).to.include({
         title: requestBody.title,
         body: requestBody.body,
         userId: requestBody.userId,
       });
+      expect(response.body.userId).to.eq(requestBody.userId);
     });
   });
-});
 
-it("PUT CALL", () => {
-  const requestBody = {
-    id: 1,
-    title: "Update Post Title",
-    body: "This is a test post created via Cypress.",
-    userId: 1,
-  };
+  it("PUT /posts/1 updates a post", () => {
+    const requestBody = {
+      id: 1,
+      title: "Update Post Title",
+      body: "This is a test post created via Cypress.",
+      userId: 1,
+    };
 
-  cy.request({
-    method: "PUT",
-    url: "https://jsonplaceholder.typicode.com/posts/1",
-    body: requestBody,
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-  }).then((response) => {
-    expect(response.status).to.eq(200);
-    expect(response.body).to.include({
-      title: requestBody.title,
-      body: requestBody.body,
+    cy.request({
+      method: "PUT",
+      url: `${baseUrl}/posts/1`,
+      body: requestBody,
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.include({
+        title: requestBody.title,
+        body: requestBody.body,
+        userId: requestBody.userId,
+      });
+      expect(response.body.userId).to.eq(requestBody.userId);
+    });
+  });
+
+  it("DELETE /posts/1 deletes a post", () => {
+    cy.request({
+      method: "DELETE",
+      url: `${baseUrl}/posts/1`,
+    }).then((response) => {
+      // JSONPlaceholder returns 200 on delete
+      expect(response.status).to.eq(200);
     });
   });
 });
